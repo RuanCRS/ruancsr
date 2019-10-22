109d38c44cb97d2db5297cdc8afdf57e07c605ff
RuanCRS <ruan.carlos.sr@gmail.com>
2019-10-21 23:14:03 -0300

    update style

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Styled from "styled-components";
 
 import ImageProfile from "../assets/profile.png";
+import ImageBackground from "../assets/banners/banner_code.jpg";
+
 import api from "../services/api";
 
 const Container = Styled.div`
@@ -46,12 +48,36 @@ const Profile = Styled.div`
 
   padding-top: 50px;
   text-align: center;
+  padding-bottom: 60px;
 
    img{
      border-radius: 50%;
-     width:550px;
-     border: 10px solid #85e085;
+     width:450px;
+     border: 8px solid;
+     border-color: #000;
+     float:left;
    }
+
+`;
+
+const BackgroundImage = Styled.div`
+
+ /* background-image: url(${ImageBackground});*/
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+
+`;
+
+const BottomDescription = Styled.div`
+  background: linear-gradient(to right,#ff8a00,#e52e71);
+  padding: 100px;
+  margin-top: -200px;
+  overflow: hidden;
+
+  p {
+    font-size: 25px;
+
+    }
 
 `;
 
@@ -61,37 +87,39 @@ class Top extends Component {
   }
   getUsers = async () => {
     try {
-      const response = await api.get(`/repos/vuejs/vue`);
+      const response = await api.get(`/repos/RuanCRS/ruancsr`);
       console.log(response.data);
-      console.log(response.data.onTimeBilling.valor);
     } catch (err) {
-      console.log(false);
       console.log(err);
     }
   };
 
   render() {
     return (
-      <Container>
-        <Menu>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/sobre">Sobre</a>
-            </li>
+      <BackgroundImage>
+        <Container>
+          <Menu>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/sobre">Sobre</a>
+              </li>
 
-            <li>
-              <a href="/contato">Contato</a>
-            </li>
-          </ul>
-        </Menu>
+              <li>
+                <a href="/contato">Contato</a>
+              </li>
+            </ul>
+          </Menu>
 
-        <Profile>
-          <img src={ImageProfile} />
-        </Profile>
-      </Container>
+          <Profile>
+            <img src={ImageProfile} />
+          </Profile>
+        </Container>
+
+        <BottomDescription></BottomDescription>
+      </BackgroundImage>
     );
   }
 }
